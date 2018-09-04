b7be8b7b9812cdb957065bf8cbc12535a0e206c3
Andrei Balbo <andreibalbo@gmail.com>
2018-09-04 11:30:32 -0300

    Improving new User form

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ Rails.application.routes.draw do
 
   resources :occurrences
 
-  resources :users
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
